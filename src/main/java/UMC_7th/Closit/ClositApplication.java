fe73f896087b12338f57f66c229372c33023e3c4
zyovn <yui2507@naver.com>
2025-01-12 00:55:46 +0900

    #2 feat: Swagger 설정 및 BaseEntity 설정

--- src/main/java/UMC_7th/Closit/ClositApplication.java
+++ src/main/java/UMC_7th/Closit/ClositApplication.java
@@ -2,8 +2,10 @@ package UMC_7th.Closit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ClositApplication {
 
 	public static void main(String[] args) {
