0e79850d34fd9aab4c725459c21a7aa04f2a594b
Junghwa <jungh150@naver.com>
2025-01-20 05:53:52 +0900

    #13 feat: Highlight 레포지토리, 컨버터 생성

---
+++ src/main/java/UMC_7th/Closit/domain/highlight/converter/HighlightConverter.java
@@ -0,0 +1,26 @@
+package UMC_7th.Closit.domain.highlight.converter;
+
+import UMC_7th.Closit.domain.highlight.dto.HighlightRequestDTO;
+import UMC_7th.Closit.domain.highlight.dto.HighlightResponseDTO;
+import UMC_7th.Closit.domain.highlight.entity.Highlight;
+import UMC_7th.Closit.domain.user.entity.User;
+
+import java.time.LocalDateTime;
+
+public class HighlightConverter {
+
+    public static HighlightResponseDTO.CreateHighlightResultDTO toCreateHighlightResultDTO(Highlight highlight) {
+        return HighlightResponseDTO.CreateHighlightResultDTO.builder()
+                .highlightId(highlight.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static Highlight toHighlight(HighlightRequestDTO.CreateHighlightDTO request, User user) {
+        return Highlight.builder()
+                .title(request.getTitle())
+                .thumbnail(request.getThumbnail())
+                .user(user)
+                .build();
+    }
+}
