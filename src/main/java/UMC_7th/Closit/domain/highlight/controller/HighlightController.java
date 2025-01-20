3c176bca58d0d114962675c5a59c538b0b7a82f5
Junghwa <jungh150@naver.com>
2025-01-20 12:12:31 +0900

    #13 feat: Highlight 컨트롤러 생성

--- src/main/java/UMC_7th/Closit/domain/highlight/controller/HighlightController.java
+++ src/main/java/UMC_7th/Closit/domain/highlight/controller/HighlightController.java
@@ -1,12 +1,25 @@
 package UMC_7th.Closit.domain.highlight.controller;
 
+import UMC_7th.Closit.domain.highlight.converter.HighlightConverter;
+import UMC_7th.Closit.domain.highlight.dto.HighlightResponseDTO;
+import UMC_7th.Closit.domain.highlight.dto.HighlightRequestDTO;
+import UMC_7th.Closit.domain.highlight.entity.Highlight;
+import UMC_7th.Closit.domain.highlight.service.HighlightCommandService;
+import UMC_7th.Closit.domain.highlight.service.HighlightQueryService;
+import UMC_7th.Closit.global.apiPayload.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/highlights")
 public class HighlightController {
+
+    private final HighlightCommandService highlightCommandService;
+    private final HighlightQueryService highlightQueryService;
 
     @Operation(summary = "하이라이트 상세 조회", description = "ID를 통해 특정 하이라이트의 상세 정보를 조회합니다.")
     @GetMapping("/{highlight_id}")
@@ -16,8 +29,9 @@ public class HighlightController {
 
     @Operation(summary = "하이라이트 생성", description = "새로운 하이라이트를 생성합니다.")
     @PostMapping
-    public ResponseEntity<String> createHighlight(@RequestBody String highlight) {
-        return ResponseEntity.ok("Created Highlight: " + highlight);
+    public ApiResponse<HighlightResponseDTO.CreateHighlightResultDTO> createHighlight(@RequestBody @Valid HighlightRequestDTO.CreateHighlightDTO request) {
+        Highlight highlight = highlightCommandService.createHighlight(request);
+        return ApiResponse.onSuccess(HighlightConverter.toCreateHighlightResultDTO(highlight));
     }
 
     @Operation(summary = "하이라이트에 게시글 추가", description = "특정 하이라이트에 게시글을 추가합니다.")
