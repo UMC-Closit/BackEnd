9508e05c7422c2cc90d87067e4aa59ade6c5a18d
Junghwa <jungh150@naver.com>
2025-01-30 03:01:54 +0900

    #19 feat: 팔로우 생성, 팔로우 삭제 api 구현

---
+++ src/main/java/UMC_7th/Closit/domain/follow/repository/FollowRepository.java
@@ -0,0 +1,8 @@
+package UMC_7th.Closit.domain.follow.repository;
+
+import UMC_7th.Closit.domain.follow.entity.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+}
