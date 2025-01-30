037c3e9150fd7e38f310c044a4882249a41b341a
Junghwa <jungh150@naver.com>
2025-01-30 20:50:44 +0900

    #19 feat: user DTO, 컨버터에 팔로우 관련 코드 추가

--- src/main/java/UMC_7th/Closit/domain/user/converter/UserConverter.java
+++ src/main/java/UMC_7th/Closit/domain/user/converter/UserConverter.java
@@ -4,12 +4,24 @@ import UMC_7th.Closit.domain.highlight.converter.HighlightConverter;
 import UMC_7th.Closit.domain.highlight.dto.HighlightResponseDTO;
 import UMC_7th.Closit.domain.highlight.entity.Highlight;
 import UMC_7th.Closit.domain.user.dto.UserResponseDTO;
+import UMC_7th.Closit.domain.user.entity.User;
 import org.springframework.data.domain.Slice;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class UserConverter {
+
+    public static UserResponseDTO.UserDTO toUserDTO(User user) {
+        return UserResponseDTO.UserDTO.builder()
+                .userId(user.getId())
+                .clositId(user.getClositId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .birth(user.getBirth())
+                .profileImage(user.getProfileImage())
+                .build();
+    }
 
     public static UserResponseDTO.UserHighlightListDTO toUserHighlightListDTO(Slice<Highlight> highlightList) {
         List<HighlightResponseDTO.HighlightDTO> highlightDTOs = highlightList.stream()
@@ -18,6 +30,26 @@ public class UserConverter {
 
         return UserResponseDTO.UserHighlightListDTO.builder()
                 .highlightList(highlightDTOs)
+                .build();
+    }
+
+    public static UserResponseDTO.UserFollwerListDTO toUserFollwerListDTO(Slice<User> followerList) {
+        List<UserResponseDTO.UserDTO> follwerDTOs = followerList.stream()
+                .map(UserConverter::toUserDTO)
+                .collect(Collectors.toList());
+
+        return UserResponseDTO.UserFollwerListDTO.builder()
+                .followerList(follwerDTOs)
+                .build();
+    }
+
+    public static UserResponseDTO.UserFollwingListDTO toUserFollwingListDTO(Slice<User> followingList) {
+        List<UserResponseDTO.UserDTO> follwingDTOs = followingList.stream()
+                .map(UserConverter::toUserDTO)
+                .collect(Collectors.toList());
+
+        return UserResponseDTO.UserFollwingListDTO.builder()
+                .followingList(follwingDTOs)
                 .build();
     }
 }
--- src/main/java/UMC_7th/Closit/domain/user/dto/UserResponseDTO.java
+++ src/main/java/UMC_7th/Closit/domain/user/dto/UserResponseDTO.java
@@ -1,11 +1,13 @@
 package UMC_7th.Closit.domain.user.dto;
 
+import UMC_7th.Closit.domain.follow.dto.FollowResponseDTO;
 import UMC_7th.Closit.domain.highlight.dto.HighlightResponseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class UserResponseDTO {
@@ -14,7 +16,36 @@ public class UserResponseDTO {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    public static class UserDTO {
+        private Long userId;
+        private String clositId;
+        private String name;
+        private String email;
+        private LocalDate birth;
+        private String profileImage;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class UserHighlightListDTO {
         private List<HighlightResponseDTO.HighlightDTO> highlightList;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserFollwerListDTO {
+        private List<UserDTO> followerList;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserFollwingListDTO {
+        private List<UserDTO> followingList;
     }
 }
