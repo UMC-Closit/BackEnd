7742411123c1e472edadf81d42a36f0e070cfba1
SeongYoon <dlatjddbs9844@yonsei.ac.kr>
2025-02-13 20:06:25 +0900

    fix: ErrorStatus의 유저 관련 에러 HttpStatus 적절한 값으로 변경, 인증 오류 시 반환 값 수정

--- src/main/java/UMC_7th/Closit/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/UMC_7th/Closit/security/jwt/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package UMC_7th.Closit.security.jwt;
 
+import UMC_7th.Closit.global.apiPayload.code.status.ErrorStatus;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,8 +15,21 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence (HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+
+        ErrorStatus errorStatus = ErrorStatus.USER_NOT_AUTHORIZED;
+//        response.setContentType("application/json");
+//        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//        response.getWriter().write("{\"error\": \"Unauthorized\", \"message\": \"" + authException.getMessage() + "\"}");
+
         response.setContentType("application/json");
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.getWriter().write("{\"error\": \"Unauthorized\", \"message\": \"" + authException.getMessage() + "\"}");
+        response.setStatus(errorStatus.getReasonHttpStatus().getHttpStatus().value());
+
+        // JSON 응답에 에러 코드, 메시지, 타임스탬프 등을 포함하여 구조화된 형태로 반환
+        String jsonResponse = "{" +
+                "\"code\": \"" + errorStatus.getCode() + "\"," +
+                "\"message\": \"" + errorStatus.getMessage() + "\"," +
+                "\"timestamp\": \"" + System.currentTimeMillis() + "\"" +
+                "}";
+        response.getWriter().write(jsonResponse);
     }
 }
