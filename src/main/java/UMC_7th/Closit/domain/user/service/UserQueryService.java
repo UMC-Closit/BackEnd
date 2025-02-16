af062a4d4a4e912d462b759ee8d9c6b1261e89be
zyovn <yui2507@naver.com>
2025-02-17 00:15:11 +0900

    #99 feat: 특정 사용자의 최근 게시물 조회

--- src/main/java/UMC_7th/Closit/domain/user/service/UserQueryService.java
+++ src/main/java/UMC_7th/Closit/domain/user/service/UserQueryService.java
@@ -1,6 +1,7 @@
 package UMC_7th.Closit.domain.user.service;
 
 import UMC_7th.Closit.domain.highlight.entity.Highlight;
+import UMC_7th.Closit.domain.post.entity.Post;
 import UMC_7th.Closit.domain.user.entity.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -16,4 +17,6 @@ public interface UserQueryService {
     User getUserInfo(String clositId);
 
     boolean isMissionDone();
+
+    Slice<Post> getRecentPostList(String clositId, Integer page); // 특정 사용자의 최근 게시글 조회
 }
