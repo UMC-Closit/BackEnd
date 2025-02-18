ad0917eedfda5d2abc39716ed5021fe0271c327e
SeongYoon <dlatjddbs9844@yonsei.ac.kr>
2025-02-18 20:42:03 +0900

    fix: 불필요한 import 삭제

--- src/main/java/UMC_7th/Closit/ClositApplication.java
+++ src/main/java/UMC_7th/Closit/ClositApplication.java
@@ -1,14 +1,9 @@
 package UMC_7th.Closit;
 
 import jakarta.annotation.PostConstruct;
-import org.joda.time.DateTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.Date;
 import java.util.TimeZone;
 
 @SpringBootApplication
